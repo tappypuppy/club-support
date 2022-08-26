9b9e608b6254d6a4c888efe108f7c6c9bdc5c0d8
tappypuppy <narita30830613@gmail.com>
2022-08-26 19:15:46 +0900

    calendar add機能追加

--- react-app/src/components/Calendar.js
+++ react-app/src/components/Calendar.js
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // pluginは、あとから
 import interactionPlugin,  { DateClickArg } from "@fullcalendar/interaction";
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import Modal from "./Modal"; //Modalコンポーネントをimportする
 
 
@@ -18,20 +18,17 @@ function AppCalendar(props) {
       }, []);
     const [day, setday] = useState();
     const [showModal, setShowModal] = useState(false);
-    // const [events,setEvents] = useState([
-    // { title: "event 1", start:"2022-08-24"},
-    // { title: "event 2", start:"2022-08-25"},
-    // { title: props.eventItems[0].event_name, start: props.eventItems[0].event_time}
-    // ])
+  
     let event_array = [
       { title: "event 1", start:"2022-08-24"},
       { title: "event 2", start:"2022-08-25"},];
     for (let step = 0; step < props.eventItems.length; step++) {
       event_array.push({ title: props.eventItems[step].event_name, start: props.eventItems[step].event_time});
     }
-    const [events,setEvents] = useState(event_array)
-    console.log("events:");
-    console.log(events);
+    // stateの更新はレンダー後らしい？
+    // const [events,setEvents] = useState(event_array)
+    // console.log("events:");
+    // console.log(events);
 
     console.log(props)
 
@@ -45,7 +42,7 @@ function AppCalendar(props) {
     plugins={[dayGridPlugin,interactionPlugin]} 
     initialView="dayGridMonth" 
     locale="ja" 
-    events={events}
+    events={event_array}
     dateClick={handleDateClick}
 
     
